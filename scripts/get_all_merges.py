e1023823386374262cb7ac2ff3ea5468a76a7533
chrishalcrow <chrishalcrow@gmail.com>
2026-09-01 08:12:37 +0100

    add all merges script

---
+++ scripts/get_all_merges.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import json
+import spikeinterface.full as si
+
+curation_decisions = {}
+
+lupin_TM_path = Path("lupin_TM_si_output")
+date_paths = list(lupin_TM_path.glob('2026*'))
+
+for date_path in date_paths:
+
+    print(f"doing {date_path}")
+
+    try:
+        analyzer_path = date_path / 'shank_2/analyzer'
+        analyzer = si.load_sorting_analyzer(analyzer_path)
+
+        merge_unit_groups = si.compute_merge_unit_groups(analyzer, preset='slay')
+        curation_decisions[date_path.name] = merge_unit_groups
+
+    except:
+        continue
+    
+
+with open("data.json", "w") as file:
+    json.dump(curation_decisions, file, default=int, indent=4)
